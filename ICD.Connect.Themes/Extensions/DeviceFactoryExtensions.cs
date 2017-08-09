2d816af24c379cf7137886f2e17d2203310a0526
Chris Cameron <chris.cameron@wedoresi.com>
2017-08-09 12:55:03 -0400

    Fixing method name

--- ICD.Connect.Themes/Extensions/DeviceFactoryExtensions.cs
+++ ICD.Connect.Themes/Extensions/DeviceFactoryExtensions.cs
@@ -6,7 +6,7 @@ namespace ICD.Connect.Themes.Extensions
 	public static class DeviceFactoryExtensions
 	{
 		[PublicAPI]
-		public static ITheme GetUiFactoryById(this IDeviceFactory factory, int id)
+		public static ITheme GetThemeById(this IDeviceFactory factory, int id)
 		{
 			return factory.GetOriginatorById<ITheme>(id);
 		}
