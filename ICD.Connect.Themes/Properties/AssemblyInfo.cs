106fc70fdede1d3e9ed71b86c7d6d96880c8c6d5
Chris Cameron <chris.cameron@wedoresi.com>
2017-08-09 12:13:12 -0400

    Initial commit

---
+++ ICD.Connect.Themes/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Themes_SimplSharp")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ICD.Connect.Themes_SimplSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
