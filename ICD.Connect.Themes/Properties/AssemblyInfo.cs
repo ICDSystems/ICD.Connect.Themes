ea58beee1454d41313c2e500df868c63dbd7ea56
Chris Cameron <chris.cameron@wedoresi.com>
2019-02-07 15:33:00 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Themes/Properties/AssemblyInfo.cs
+++ ICD.Connect.Themes/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Themes")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Themes")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
 [assembly: AssemblyVersion("4.0.0.0")]
