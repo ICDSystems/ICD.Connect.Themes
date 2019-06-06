034b2e92bd5e237c618e00ebbea76ddb3200805a
Chris Cameron <chris.cameron@wedoresi.com>
2019-06-06 16:48:01 -0400

    fix: Potential fixes for multiple subscription

--- ICD.Connect.Themes/AbstractTheme.cs
+++ ICD.Connect.Themes/AbstractTheme.cs
@@ -9,8 +9,8 @@ namespace ICD.Connect.Themes
 	public abstract class AbstractTheme<TSettings> : AbstractOriginator<TSettings>, ITheme
 		where TSettings : AbstractThemeSettings, new()
 	{
-		private bool m_EnvironmentInitialized;
 		private bool m_CoreSettingsApplied;
+		private ICore m_Core;
 
 		/// <summary>
 		/// Clears the instantiated user interfaces.
@@ -33,16 +33,23 @@ namespace ICD.Connect.Themes
 		}
 
 		/// <summary>
+		/// Constructor.
+		/// </summary>
+		protected AbstractTheme()
+		{
+			IcdEnvironment.OnProgramInitializationComplete += IcdEnvironmentOnProgramInitializationComplete;
+		}
+
+		/// <summary>
 		/// Override to apply settings to the instance.
 		/// </summary>
 		/// <param name="settings"></param>
 		/// <param name="factory"></param>
 		protected override void ApplySettingsFinal(TSettings settings, IDeviceFactory factory)
 		{
-			base.ApplySettingsFinal(settings, factory);
+			SetCore(ServiceProvider.TryGetService<ICore>());
 
-			IcdEnvironment.OnProgramInitializationComplete += IcdEnvironmentOnProgramInitializationComplete;
-			ServiceProvider.GetService<ICore>().OnSettingsApplied += CoreOnSettingsApplied;
+			base.ApplySettingsFinal(settings, factory);
 
 			BuildUserInterfaces();
 		}
@@ -60,28 +67,75 @@ namespace ICD.Connect.Themes
 
 		protected virtual void ActivateUserInterfaces()
 		{
-			
+		}
+
+		#region Program Initialization Callbacks
+
+		private void SetCore(ICore core)
+		{
+			if (core == m_Core)
+				return;
+
+			Unsubscribe(m_Core);
+
+			m_Core = core;
+			m_CoreSettingsApplied = false;
+
+			Subscribe(m_Core);
+		}
+
+		/// <summary>
+		/// Subscribe to the core events.
+		/// </summary>
+		/// <param name="core"></param>
+		private void Subscribe(ICore core)
+		{
+			if (core == null)
+				return;
+
+			core.OnSettingsApplied += CoreOnSettingsApplied;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the core events.
+		/// </summary>
+		/// <param name="core"></param>
+		private void Unsubscribe(ICore core)
+		{
+			if (core == null)
+				return;
+
+			core.OnSettingsApplied -= CoreOnSettingsApplied;
+		}
+
+		/// <summary>
+		/// Called when the Core finishes applying settings.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="eventArgs"></param>
+		private void CoreOnSettingsApplied(object sender, EventArgs eventArgs)
+		{
+			m_CoreSettingsApplied = true;
+
+			ActivateUserInterfacesIfReady();
+		}
+
+		/// <summary>
+		/// Called when the IcdEnvironment initialization state changes.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="eventArgs"></param>
+		private void IcdEnvironmentOnProgramInitializationComplete(object sender, EventArgs eventArgs)
+		{
+			ActivateUserInterfacesIfReady();
 		}
 
 		private void ActivateUserInterfacesIfReady()
 		{
-			if (!m_CoreSettingsApplied || !m_EnvironmentInitialized)
-				return;
-
-			ActivateUserInterfaces();
+			if (m_CoreSettingsApplied && IcdEnvironment.ProgramIsInitialized)
+				ActivateUserInterfaces();
 		}
 
-		private void CoreOnSettingsApplied(object sender, EventArgs eventArgs)
-		{
-			m_CoreSettingsApplied = true;
-			ActivateUserInterfacesIfReady();
-		}
-
-		private void IcdEnvironmentOnProgramInitializationComplete(object sender, EventArgs eventArgs)
-		{
-			m_EnvironmentInitialized = true;
-			ActivateUserInterfacesIfReady();
-		}
-
+		#endregion
 	}
 }
