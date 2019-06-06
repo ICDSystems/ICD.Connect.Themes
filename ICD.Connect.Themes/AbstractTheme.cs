fa425ac31aba91290dc1dffae877979deef7ce9e
Jack Kanarish <jack.kanarish@wedoresi.com>
2019-06-06 12:04:06 -0400

    feat: add concept of "activating" a user interface, indicating it is ready for human interaction

--- ICD.Connect.Themes/AbstractTheme.cs
+++ ICD.Connect.Themes/AbstractTheme.cs
@@ -1,4 +1,7 @@
-﻿using ICD.Connect.Settings;
+﻿using System;
+using ICD.Common.Utils;
+using ICD.Common.Utils.Services;
+using ICD.Connect.Settings;
 using ICD.Connect.Settings.Core;
 
 namespace ICD.Connect.Themes
@@ -6,6 +9,9 @@ namespace ICD.Connect.Themes
 	public abstract class AbstractTheme<TSettings> : AbstractOriginator<TSettings>, ITheme
 		where TSettings : AbstractThemeSettings, new()
 	{
+		private bool m_EnvironmentInitialized;
+		private bool m_CoreSettingsApplied;
+
 		/// <summary>
 		/// Clears the instantiated user interfaces.
 		/// </summary>
@@ -35,6 +41,9 @@ namespace ICD.Connect.Themes
 		{
 			base.ApplySettingsFinal(settings, factory);
 
+			IcdEnvironment.OnProgramInitializationComplete += IcdEnvironmentOnProgramInitializationComplete;
+			ServiceProvider.GetService<ICore>().OnSettingsApplied += CoreOnSettingsApplied;
+
 			BuildUserInterfaces();
 		}
 
@@ -48,5 +57,31 @@ namespace ICD.Connect.Themes
 
 			ClearUserInterfaces();
 		}
+
+		protected virtual void ActivateUserInterfaces()
+		{
+			
+		}
+
+		private void ActivateUserInterfacesIfReady()
+		{
+			if (!m_CoreSettingsApplied || !m_EnvironmentInitialized)
+				return;
+
+			ActivateUserInterfaces();
+		}
+
+		private void CoreOnSettingsApplied(object sender, EventArgs eventArgs)
+		{
+			m_CoreSettingsApplied = true;
+			ActivateUserInterfacesIfReady();
+		}
+
+		private void IcdEnvironmentOnProgramInitializationComplete(object sender, EventArgs eventArgs)
+		{
+			m_EnvironmentInitialized = true;
+			ActivateUserInterfacesIfReady();
+		}
+
 	}
 }
