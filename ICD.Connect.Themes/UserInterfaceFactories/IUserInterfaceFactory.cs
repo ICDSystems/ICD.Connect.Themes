e1f40a94a3757dc7b8897b3de2faee28bb019aaf
Chris Cameron <chris.cameron@wedoresi.com>
2019-06-07 12:55:42 -0400

    feat: Moving UI and UI Factory interfaces and abstractions into Themes project

---
+++ ICD.Connect.Themes/UserInterfaceFactories/IUserInterfaceFactory.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using ICD.Connect.Partitioning.Rooms;
+using ICD.Connect.Themes.UserInterfaces;
+
+namespace ICD.Connect.Themes.UserInterfaceFactories
+{
+	public interface IUserInterfaceFactory
+	{
+		/// <summary>
+		/// Disposes the instantiated UIs.
+		/// </summary>
+		void Clear();
+
+		/// <summary>
+		/// Instantiates the user interfaces for the originators in the core.
+		/// </summary>
+		/// <returns></returns>
+		void BuildUserInterfaces();
+
+		/// <summary>
+		/// Gets the instantiated user interfaces.
+		/// </summary>
+		IEnumerable<IUserInterface> GetUserInterfaces();
+
+		/// <summary>
+		/// Assigns the rooms to the existing user interfaces.
+		/// </summary>
+		void ReassignUserInterfaces();
+
+		/// <summary>
+		/// Assigns the rooms to the existing user interfaces.
+		/// </summary>
+		void AssignUserInterfaces(IEnumerable<IRoom> rooms);
+
+		/// <summary>
+		/// Activates this user interface.
+		/// </summary>
+		void ActivateUserInterfaces();
+	}
+}
---
+++ ICD.Connect.Themes/UserInterfaces/AbstractUserInterface.cs
@@ -0,0 +1,47 @@
+﻿using ICD.Common.Utils;
+using ICD.Connect.Partitioning.Rooms;
+
+namespace ICD.Connect.Themes.UserInterfaces
+{
+	public abstract class AbstractUserInterface : IUserInterface
+	{
+		/// <summary>
+		/// Gets the room attached to this UI.
+		/// </summary>
+		public abstract IRoom Room { get; }
+
+		/// <summary>
+		/// Gets the target instance attached to this UI.
+		/// </summary>
+		public abstract object Target { get; }
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		public abstract void Dispose();
+
+		/// <summary>
+		/// Updates the UI to represent the given room.
+		/// </summary>
+		/// <param name="room"></param>
+		public abstract void SetRoom(IRoom room);
+
+		/// <summary>
+		/// Tells the UI that it should be considered ready to use.
+		/// For example updating the online join on a panel or starting a long-running process that should be delayed.
+		/// </summary>
+		public abstract void Activate();
+
+		/// <summary>
+		/// Gets the string representation for this instance.
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			ReprBuilder builder = new ReprBuilder(this);
+			builder.AppendProperty("Room", Room);
+			builder.AppendProperty("Target", Target);
+			return builder.ToString();
+		}
+	}
+}
