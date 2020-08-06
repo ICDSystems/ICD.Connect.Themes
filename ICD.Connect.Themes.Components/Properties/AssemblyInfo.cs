1f5d62e504df8f510e09dd58ce41ed1ebdf03b70
Chris Cameron <chris.cameron@wedoresi.com>
2020-08-06 12:09:26 -0400

    chore: Updating changelog, incrementing patch version

--- ICD.Connect.Themes.Components/Properties/AssemblyInfo.cs
+++ ICD.Connect.Themes.Components/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Themes.Components")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
-[assembly: AssemblyVersion("5.1.0.0")]
+[assembly: AssemblyVersion("5.1.1.0")]
