a81b74d78490e45a66b5c89ae98f34aa4594c1d1
Chris Cameron <chris.cameron@icdsystems.com>
2022-01-05 16:27:18 -0500

    chore: Updating AssemblyInfo year

--- ICD.Connect.Themes.Components/Properties/AssemblyInfo.cs
+++ ICD.Connect.Themes.Components/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Themes.Components")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Themes.Components")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
 [assembly: AssemblyVersion("6.1.1.0")]
--- ICD.Connect.Themes/Properties/AssemblyInfo.cs
+++ ICD.Connect.Themes/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Themes")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Themes")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
 [assembly: AssemblyVersion("6.1.1.0")]
