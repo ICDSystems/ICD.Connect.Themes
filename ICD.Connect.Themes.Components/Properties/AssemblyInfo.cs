d0d1d256daa102a31310e7ece28710e3b069cb68
Drew Tingen <drew.tingen@icdsystems.com>
2022-07-01 15:08:49 -0400

    chore: Update changelog, increment assembly minor version

--- ICD.Connect.Themes.Components/Properties/AssemblyInfo.cs
+++ ICD.Connect.Themes.Components/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Themes.Components")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
-[assembly: AssemblyVersion("6.2.0.0")]
+[assembly: AssemblyVersion("6.3.0.0")]
