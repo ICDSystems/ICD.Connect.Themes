89f13586e994a30b7470f14866c2c6f242e8380d
Drew Tingen <drew.tingen@icdsystems.com>
2022-12-24 14:37:49 -0500

    chore: Update changelog, increment assembly minor version

--- ICD.Connect.Themes.Crosspoints/Properties/AssemblyInfo.cs
+++ ICD.Connect.Themes.Crosspoints/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Themes.Crosspoints")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
-[assembly: AssemblyVersion("6.3.0.0")]
+[assembly: AssemblyVersion("6.4.0.0")]
