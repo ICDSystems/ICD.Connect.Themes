51bbc3f2bccabb2df11c8a4196166cfa62551659
Drew Tingen <drew.tingen@icdsystems.com>
2023-03-22 08:18:35 -0400

    chore: Update copyright year

--- ICD.Connect.Themes.Crosspoints/Properties/AssemblyInfo.cs
+++ ICD.Connect.Themes.Crosspoints/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Themes.Crosspoints")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Themes.Crosspoints")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2023")]
 [assembly: AssemblyVersion("6.4.1.0")]
